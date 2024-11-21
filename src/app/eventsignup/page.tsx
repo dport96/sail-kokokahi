f29e946e724fc185f586db867e157daacbb83e0d
Ethan Christman <ec72@hawaii.edu>
2024-11-21 01:46:29 -1000

    finished eventsignup mockup

---
+++ src/app/eventsignup/page.tsx
@@ -0,0 +1,55 @@
+import { getServerSession } from 'next-auth';
+import { Col, Container, Row, DropdownButton } from 'react-bootstrap';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import authOptions from '@/lib/authOptions';
+import { prisma } from '@/lib/prisma';
+
+/** Render a list of stuff for the logged in user. */
+const EventSignUpPage = async () => {
+  // Protect the page, only logged in users can access it.
+  const session = await getServerSession(authOptions);
+  loggedInProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+      // eslint-disable-next-line @typescript-eslint/comma-dangle
+    } | null,
+  );
+  const events = await prisma.event.findMany({});
+
+  // console.log(stuff);
+  return (
+    <main>
+      <Container>
+        <Row>
+          <Col>
+            <div className="mb-3">
+              <h2>Event Sign-up</h2>
+              {events.map((event) => (
+                <Row key={event.id} className="border p-3">
+                  <h4>{event.date}</h4>
+                  <h5>{event.title}</h5>
+                  <DropdownButton title="Information" variant="light">
+                    Time:
+                    {' '}
+                    {event.time}
+                    <br />
+                    Potential Hours:
+                    {' '}
+                    {event.hours}
+                    <br />
+                    Description:
+                    {' '}
+                    {event.description}
+                    <br />
+                  </DropdownButton>
+                </Row>
+              ))}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default EventSignUpPage;
