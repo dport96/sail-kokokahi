a1bf43a150303c1239a9c74c973d082b0e9707e9
Ethan Christman <ec72@hawaii.edu>
2024-11-21 14:14:52 -1000

    changed landing page to request users sign in

---
+++ src/app/member-landingpage/page.tsx
@@ -0,0 +1,30 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+/** The Home page. */
+const MemberLandingPage = () => (
+  <main>
+    <Container className="background centered" id="landing-page" fluid>
+      <Row>
+        <h1 className="text-white">Welcome! please select an action below</h1>
+      </Row>
+      <Row>
+        <Col>
+          <button type="button" className="m-3">
+            <a href="/member-dashboard">Member Dashboard</a>
+          </button>
+        </Col>
+        <Col>
+          <button type="button" className="m-3">
+            <a href="/eventsignup">
+              Event
+              <br />
+              Sign-up
+            </a>
+          </button>
+        </Col>
+      </Row>
+    </Container>
+  </main>
+);
+
+export default MemberLandingPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 /** The Home page. */
 const Home = () => (
   <main>
     <Container className="background centered" id="landing-page" fluid>
       <Row>
-        <h1 className="text-white">Welcome! please select an action below</h1>
+        <h1 className="text-white">Welcome!</h1>
       </Row>
       <Row>
-        <Col>
-          <button type="button" className="m-3">
-            <a href="/member-dashboard">Member Dashboard</a>
-          </button>
-        </Col>
-        <Col>
-          <button type="button" className="m-3">
-            <a href="/eventsignup">
-              Event
-              <br />
-              Sign-up
-            </a>
-          </button>
-        </Col>
+        <h1 className="text-white">Please click below to sign in.</h1>
+        <button type="button" className="m-3">
+          <a href="/auth/signin">here</a>
+        </button>
       </Row>
     </Container>
   </main>
