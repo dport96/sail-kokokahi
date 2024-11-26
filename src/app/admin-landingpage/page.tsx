f443fc1947df65f33d7a49283b5b542fd9bed797
Ethan Christman <ec72@hawaii.edu>
2024-11-26 11:22:47 -1000

    Finished Issues 24 and 29
    Adjusted initial landing page to request sign in. Created directory page with admin and member buttons, which redirect to admin and member landing pages. Added member protection page on page-protection.ts in order to protect admins from viewing members only pages.

--- src/app/admin-landingpage/page.tsx
+++ src/app/admin-landingpage/page.tsx
@@ -1,30 +1,41 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/lib/authOptions';
+import { adminProtectedPage } from '@/lib/page-protection';
 
-/** The Home page. */
-const AdminLandingPage = () => (
-  <main>
-    <Container className="background centered" id="landing-page" fluid>
-      <Row>
-        <h1 className="text-white">Welcome! please select an action below</h1>
-      </Row>
-      <Row>
-        <Col>
-          <button type="button" className="m-3">
-            <a href="/member-dashboard">Member Dashboard</a>
-          </button>
-        </Col>
-        <Col>
-          <button type="button" className="m-3">
-            <a href="/eventsignup">
-              Event
-              <br />
-              Sign-up
-            </a>
-          </button>
-        </Col>
-      </Row>
-    </Container>
-  </main>
-);
+const AdminLandingPage = async () => {
+  const session = await getServerSession(authOptions);
+  adminProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+    } | null,
+  );
+
+  return (
+    <main>
+      <Container className="background centered" id="landing-page" fluid>
+        <Row>
+          <h1 className="text-white">Welcome! please select an action below</h1>
+        </Row>
+        <Row>
+          <Col>
+            <button type="button" className="m-3">
+              <a href="/admin-dashboard">Admin Dashboard</a>
+            </button>
+          </Col>
+          {/* <Col>
+            <button type="button" className="m-3">
+              <a href="/eventsignup">
+                Event
+                <br />
+                Sign-up
+              </a>
+            </button>
+          </Col> */}
+        </Row>
+      </Container>
+    </main>
+  );
+};
 
 export default AdminLandingPage;
--- src/app/member-landingpage/page.tsx
+++ src/app/member-landingpage/page.tsx
@@ -1,30 +1,42 @@
 import { Container, Row, Col } from 'react-bootstrap';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/lib/authOptions';
+import { memberProtectedPage } from '@/lib/page-protection';
 
 /** The Home page. */
-const MemberLandingPage = () => (
-  <main>
-    <Container className="background centered" id="landing-page" fluid>
-      <Row>
-        <h1 className="text-white">Welcome! please select an action below</h1>
-      </Row>
-      <Row>
-        <Col>
-          <button type="button" className="m-3">
-            <a href="/member-dashboard">Member Dashboard</a>
-          </button>
-        </Col>
-        <Col>
-          <button type="button" className="m-3">
-            <a href="/eventsignup">
-              Event
-              <br />
-              Sign-up
-            </a>
-          </button>
-        </Col>
-      </Row>
-    </Container>
-  </main>
-);
+const MemberLandingPage = async () => {
+  const session = await getServerSession(authOptions);
+  memberProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+    } | null,
+  );
+
+  return (
+    <main>
+      <Container className="background centered" id="landing-page" fluid>
+        <Row>
+          <h1 className="text-white">Welcome! please select an action below</h1>
+        </Row>
+        <Row>
+          <Col>
+            <button type="button" className="m-3">
+              <a href="/member-dashboard">Member Dashboard</a>
+            </button>
+          </Col>
+          <Col>
+            <button type="button" className="m-3">
+              <a href="/eventsignup">
+                Event
+                <br />
+                Sign-up
+              </a>
+            </button>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
 
 export default MemberLandingPage;
